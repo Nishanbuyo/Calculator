5c2d1e99337fed2dbf6c73a24dddf12cdb29bf1d
Nishanbuyo <nishanbuyo05@gmail.com>
2020-05-19 20:57:29 +0545

    Updates

---
+++ precache-manifest.18e8c4d4e096cb6f5fd6e22eef58a49e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4169f33f01725bbb1e6e95e1aa81c8e7",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "cb57574e86c2b49a5dc9",
+    "url": "/Calculator/static/css/main.e109ff72.chunk.css"
+  },
+  {
+    "revision": "d970827df4dfb077deaa",
+    "url": "/Calculator/static/js/2.77c47ad3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Calculator/static/js/2.77c47ad3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb57574e86c2b49a5dc9",
+    "url": "/Calculator/static/js/main.37e6ecb7.chunk.js"
+  },
+  {
+    "revision": "36cda1db55e6e62de084",
+    "url": "/Calculator/static/js/runtime-main.c4967e62.js"
+  }
+]);
